a757d9bac9c8442a5a9855fdfacb42f4a56669a9
Zhans2910 <zsatui9@gmail.com>
2020-06-13 15:14:48 +0600

    new

--- src/test/java/simplejavacalculator/CalculatorTest.java
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -16,4 +16,24 @@ public class CalculatorTest {
         Double result = c.calculateMono(Calculator.MonoOperatorModes.squareRoot, 25.0);
         Assert.assertEquals((Double) 5.0, result);
     }
+    @Test
+    public void Test1() throws Exception{
+        Calculator c = new Calculator();
+        Double result = c.calculateMono(Calculator.MonoOperatorModes.oneDevidedBy, 25.0);
+        Assert.assertEquals((Double) 0.04, result);
+    }
+    @Test
+    public void Test2() throws Exception{
+        Calculator c = new Calculator();
+        Double result = c.calculateMono(Calculator.MonoOperatorModes.sin, 0.0);
+        Assert.assertEquals((Double) 0.0, result);
+    }
+    @Test
+    public void Test3() throws Exception{
+        Calculator c = new Calculator();
+        c.calculateBi(Calculator.BiOperatorModes.add,3.0);
+        Double result = c.calculateBi(Calculator.BiOperatorModes.normal,5.0 );
+        Assert.assertEquals((Double) 8.0, result);
+    }
+
 }
