058ac61c3bc5a11380021251ab56807ffbd06614
Zhans2910 <zsatui9@gmail.com>
2020-06-15 20:17:29 +0600

    new new

--- src/test/java/simplejavacalculator/CalculatorTest.java
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -99,8 +99,8 @@ public class CalculatorTest {
     @Test
     public void Test15() throws Exception {
         Calculator c = new Calculator();
-        c.calculateBi(Calculator.BiOperatorModes.NORMAL, 2.0);
-        Assert.assertTrue(Double.isNaN(c.calculateBi(Calculator.BiOperatorModes.NORMAL, 3.0)));
+        c.calculateBi(Calculator.BiOperatorModes.NORMAL, 5.0);
+        Assert.assertTrue(Double.isNaN(c.calculateBi(Calculator.BiOperatorModes.NORMAL, 8.0)));
     }
     @Test
     public void Test16() throws Exception {
@@ -108,6 +108,32 @@ public class CalculatorTest {
         c.calculateBi(Calculator.BiOperatorModes.X_POWER_OF_Y, 2.0);
         Double result= c.calculateBi(Calculator.BiOperatorModes.NORMAL, 3.0);
         Assert.assertEquals( (Double) 8.0, result);
+    }
+    @Test
+    public void Test17() throws Exception {
+        Calculator c = new Calculator();
+        c.calculateBi(Calculator.BiOperatorModes.ADD,3.0);
+        Double result = c.calculateBi(Calculator.BiOperatorModes.NORMAL,0.0 );
+        Assert.assertEquals((Double) 3.0, result);
+    }
+    @Test
+    public void Test18() throws Exception {
+            Calculator calculator = new Calculator();
+            calculator.calculateBi(Calculator.BiOperatorModes.ADD, 8.3);
+            calculator.calculateBi(Calculator.BiOperatorModes.ADD, 2.0);
+            Assert.assertEquals((Double)13.3, calculator.calculateEqual(3.0));
+    }
+    @Test
+    public void Test19() throws Exception {
+        Calculator c = new Calculator();
+        Double calculation = c.calculateMono(Calculator.MonoOperatorModes.TAN, -30.0);
+        Assert.assertEquals((Double) 6.405331196646276, calculation);
+    }
+    @Test
+    public void Test20() throws Exception {
+        Calculator c = new Calculator();
+        c.calculateBi(Calculator.BiOperatorModes.NORMAL, 0.0);
+        Assert.assertTrue(Double.isNaN(c.reset()));
     }
 }
 
